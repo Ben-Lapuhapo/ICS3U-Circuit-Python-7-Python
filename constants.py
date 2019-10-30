d11ecc73e9034b7abf8f1baf01bf6609744c074d
Ben Lapuhapo <ben.lapuhapo@mths.ca>
2019-10-30 01:02:36 +0000

    Added Start Menu to pygame

---
+++ constants.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+# Created by: Ben Lapuhapo
+# Created on: OCT 2019
+# This module contains constants for the alien game 
+
+# CircuitPython screen size is 160x120 and sprites are 16x16
+SCREEN_X = 160
+SCREEN_Y = 120
+SCREEN_GRID_X = 16
+SCREEN_GRID_Y = 8
+SPRITE_SIZE = 16
+TOTAL_NUMBER_OF_ALIENS = 5
+FPS = 60
+SPRITE_MOVEMENT_SPEED = 1
+
+# Using for Button State 
+button_state = {
+    "button_up": "up",
+    "button_just_pressed": "just pressed",
+    "button_still_pressed": "still pressed",
+    "button_released": "released"
+}
